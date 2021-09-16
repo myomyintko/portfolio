92a048b9ee55e098552287e63704f24c85c971ce
MyoMyintKo <myomyintko.developer@gmail.com>
2021-09-16 14:45:59 +0630

    fixed scroll

--- js/script.js
+++ js/script.js
@@ -2,6 +2,35 @@ $(window).on("load", () => {
   $(".loader").fadeOut();
   $("#preloder").delay(100).fadeOut("slower");
 });
+
+function onScroll() {
+  var scrollPos = $(document).scrollTop();
+  $(".nav-link").each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    console.log('scrollpos '+scrollPos);
+    console.log('top '+refElement.position().top);
+    console.log('height '+refElement.height());
+    if (
+      refElement.position().top - 100 <= scrollPos
+    ) {
+      $(".nav-link").removeClass("active");
+      currLink.addClass("active");
+    } else {
+      currLink.removeClass("active");
+    }
+  });
+}
+
+$(document).on("scroll", () => {
+  onScroll();
+  if ($(window).scrollTop() > 100) {
+    $("header").addClass("shadow-sm");
+  } else {
+    $("header").removeClass("shadow-sm");
+  }
+});
+
 $(document).ready(function () {
   // Mobile navbar
   $(".open-menu").on("click", () => {
@@ -11,17 +40,8 @@ $(document).ready(function () {
   $(".mobile-nav-link").on("click", () => {
     $("#mobile-menu").addClass("hidden");
   });
-$("#close-menu").on("click", () => {
+  $("#close-menu").on("click", () => {
     $("#mobile-menu").addClass("hidden");
-  });
-
-  $(document).on("scroll", () => {
-    onScroll;
-    if ($(window).scrollTop() > 100) {
-      $("header").addClass("shadow-sm");
-    } else {
-      $("header").removeClass("shadow-sm");
-    }
   });
 
   //smoothscroll
@@ -51,60 +71,3 @@ $("#close-menu").on("click", () => {
       );
   });
 });
-
-function onScroll() {
-  var scrollPos = $(document).scrollTop();
-  $(".nav-link").each(function () {
-    var currLink = $(this);
-    var refElement = $(currLink.attr("href"));
-    if (
-      refElement.position().top <= scrollPos &&
-      refElement.position().top + refElement.height() > scrollPos
-    ) {
-      $(".nav-link").removeClass("active");
-      currLink.addClass("active");
-    } else {
-      currLink.removeClass("active");
-    }
-  });
-}
-
-// var form = $("#contact-me");
-
-// function alert(text) {
-//   $("mail-alert").removeClass("hidden");
-//   $("mail-status-text").html(text);
-// }
-
-// $(".close-alert").click(() => {
-//   $("mail-alert").addClass("hidden");
-// });
-
-// $(".send-mail").on("click", (event) => {
-//   event.preventDefault();
-//   var obj = {
-//     firstname: $("[name=firstname]").val(),
-//     lastname: $("[name=lastname]").val(),
-//     _replyto: $("[name=_replyto]").val(),
-//     subject: $("[name=subject]").val(),
-//     message: $("[name=message]").val(),
-//   };
-//   if(obj.firstname > 0){
-
-//   }
-//   // fetch("https://formspree.io/f/xyyldkkg", {
-//   //   method: "POST",
-//   //   dataType: "json",
-//   //   data: obj,
-//   //   headers: {
-//   //     Accept: "application/json",
-//   //   },
-//   // })
-//   //   .then((response) => {
-//   //     alert("Thanks for send submitting");
-//   //     form.reset();
-//   //   })
-//   //   .catch((error) => {
-//   //     alert('"Oops! There was a problem submitting your form');
-//   //   });
-// });
