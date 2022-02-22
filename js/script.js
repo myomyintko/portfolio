354fff4a51f17da78c82facd6db4f45cbf57d789
Marco <myomyintko.developer@gmail.com>
2022-02-22 16:24:34 +0630

    fix data

--- js/script.js
+++ js/script.js
@@ -1,19 +1,65 @@
 $(window).on("load", () => {
   $(".loader").fadeOut();
   $("#preloder").delay(100).fadeOut("slower");
+  loadSkills();
+  loadExp();
 });
+
+function loadSkills(){
+  $.getJSON("./../data/skill.json", function (skills) {
+    var items = [];
+    $.each(skills, function (key) {
+      $.each(skills[key], (k,v) => {
+        items.push(v)        
+      });
+      items = items.filter((item,index) => items.indexOf(item) === index)
+    });
+    $.each(items, (i,v)=>{
+      $("<span class='relative w-auto px-2 py-1 border bg-blue-800 text-white cursor-pointer rounded m-2 font-normal uppercase text-sm text-center' id='" + i + "'>" + v + "</span>")
+   .appendTo( "#skills" )
+ })
+  });
+}
+
+
+function loadExp(){
+  $.getJSON("./../data/exp.json", function (exps) {
+    $.each(exps, function (key) {
+      $.each(exps[key], (k,v) => {
+            $(`
+          <div class="exp">
+           <h3 class="
+                 inline-block
+                 relative
+                 text-sm
+                 py-1
+                 px-3
+                 tracking-wider
+                 uppercase
+               ">
+             ${v.date_from} - ${v.date_to}
+           </h3>
+           <h4 class="text-md font-semibold uppercase">${v.position}</h4>
+           <h5 class="text-lg uppercase font-light">
+             ${v.company_name}
+           </h5>
+           <p>
+             ${v.descriptions}
+           </p>
+         </div>
+          `)
+       .appendTo( "#exps" )
+      });
+    });
+  });
+}
 
 function onScroll() {
   var scrollPos = $(document).scrollTop();
   $(".nav-link").each(function () {
     var currLink = $(this);
     var refElement = $(currLink.attr("href"));
-    console.log('scrollpos '+scrollPos);
-    console.log('top '+refElement.position().top);
-    console.log('height '+refElement.height());
-    if (
-      refElement.position().top - 100 <= scrollPos
-    ) {
+    if (refElement.position().top - 100 <= scrollPos) {
       $(".nav-link").removeClass("active");
       currLink.addClass("active");
     } else {
